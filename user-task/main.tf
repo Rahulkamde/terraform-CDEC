cd87bed3e5a07b9a2db2af7177983e114e21dc69
Golya <Rahulkamde@gmail.com>
2023-08-07 13:56:34 +0530

    create-new-user

---
+++ user-task/main.tf
@@ -0,0 +1,64 @@
+resource "aws_iam_group_membership" "demo" {
+  name = "members"
+
+  users = [
+    aws_iam_user.user_one.name,
+    aws_iam_user.user_two.name,
+    aws_iam_user.user_three.name,
+    aws_iam_user.user_four.name,
+    aws_iam_user.user_five.name
+  ]
+
+  group = aws_iam_group.group.name
+}
+
+
+resource "aws_iam_group" "group" {
+  name = "Cloudblitz"
+}
+
+
+resource "aws_iam_user" "user_one" {
+  name = "task-user-one"
+  tags = {
+    name      = "rahulk"
+    purpose   = "terraform-user-create"
+    startdate = "06/08/2023" 
+  }
+}
+
+resource "aws_iam_user" "user_two" {
+  name = "task-user-two"
+  tags = {
+    name      = "rahulk"
+    purpose   = "terraform-user-create"
+    startdate = "06/08/2023" 
+  }
+}
+
+resource "aws_iam_user" "user_three" {
+  name = "task-user-three"
+  tags = {
+    name      = "rahulk"
+    purpose   = "terraform-user-create"
+    startdate = "06/08/2023" 
+  }
+}
+
+resource "aws_iam_user" "user_four" {
+  name = "task-user-four"
+  tags = {
+    name      = "rahulk"
+    purpose   = "terraform-user-create"
+    startdate = "06/08/2023" 
+  }
+}
+
+resource "aws_iam_user" "user_five" {
+  name = "task-user-five"
+  tags = {
+    name      = "rahulk"
+    purpose   = "terraform-user-create"
+    startdate = "06/08/2023" 
+  }
+}
---
+++ user-task/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.11.0"
+    }
+  }
+}
+
+ provider "aws" {
+      region     = "us-west-1"
+      access_key = "AKIA4TIFA7FQ46XUYMWE"
+      secret_key = "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"
+    }
